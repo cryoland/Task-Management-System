8d51315c0de87c068e8a0bf2771dc5c1dbcca4bf
v.konov <v.konov@itransition.com>
2020-02-04 17:37:30 +0300

    new features for forms

--- TMS/ViewModels/LoginModel.cs
+++ TMS/ViewModels/LoginModel.cs
@@ -8,9 +8,11 @@ namespace TMS.ViewModels
 {
     public class LoginModel
     {
+        [Display(Name = "Введите Email")]
         [Required(ErrorMessage = "Не указан Email")]
         public string Email { get; set; }
 
+        [Display(Name = "Введите пароль")]
         [Required(ErrorMessage = "Не указан пароль")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
--- TMS/ViewModels/RegisterModel.cs
+++ TMS/ViewModels/RegisterModel.cs
@@ -8,18 +8,24 @@ namespace TMS.ViewModels
 {
     public class RegisterModel
     {
+        [Display(Name = "Введите Email")]
         [Required(ErrorMessage = "Не указан Email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Display(Name = "Введите ShortName")]
         public string ShortName { get; set; }
 
+        [Display(Name = "Введите ФИО")]
         [Required(ErrorMessage = "Не указаны ФИО")]
         public string FullName { get; set; }
 
+        [Display(Name = "Введите пароль")]
         [Required(ErrorMessage = "Не указан пароль")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Display(Name = "Повторите пароль")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Пароль введен неверно")]
         public string ConfirmPassword { get; set; }
