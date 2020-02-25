cf34a162fcc77caf4d205f12465191725c5d424f
Cryoland <vsrknv@gmail.com>
2020-02-25 18:13:07 +0300

    Employee controller and views added

---
+++ TMS.WebUI/Controllers/ApiController.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TMS.WebUI.Controllers
+{
+    public abstract class ApiController : Controller
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
---
+++ TMS.WebUI/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TMS.Application.Employees.Commands.CreateEmployee;
+using TMS.Application.Employees.Queries.GetEmployeeDetail;
+using TMS.Application.Employees.Queries.GetEmployeeList;
+
+namespace TMS.WebUI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<EmployeeListVm>> Index()
+        {            
+            return View(await Mediator.Send(new GetEmployeeListQuery()));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EmployeeDetailVm>> Detailed(int id)
+        {
+            return await Mediator.Send(new GetEmployeeDetailQuery { EmployeeId = id });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EmployeeDetailVm>> Edit(int id)
+        {
+            return await Mediator.Send(new GetEmployeeDetailQuery { EmployeeId = id });
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new { });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<long>> Create(CreateEmployeeCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
