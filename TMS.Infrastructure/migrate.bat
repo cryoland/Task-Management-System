40adef53c2087b8d9e5bc1edc5181834d7ef7473
Cryoland <vsrknv@gmail.com>
2020-02-25 15:45:50 +0300

    Migrations assembler script modified: new exec project

--- TMS.Infrastructure/migrate.bat
+++ TMS.Infrastructure/migrate.bat
@@ -1,7 +1,7 @@
 ﻿@echo off
 
 set datetimef=%date:~-4%-%date:~3,2%-%date:~0,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
-set exec_proj=TMS.WebUI-test
+set exec_proj=TMS.WebUI
 
 REM Creating migrations
 dotnet ef migrations add Migration_%datetimef% --startup-project ../%exec_proj%/%exec_proj%.csproj --context TMS.Infrastructure.Persistence.ApplicationDbContext -o Persistence/Migrations
