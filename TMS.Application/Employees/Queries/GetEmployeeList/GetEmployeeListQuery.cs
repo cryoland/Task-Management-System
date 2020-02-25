7411a10693f8ad0fad58a7b86719b5b3ff2d424f
Cryoland <vsrknv@gmail.com>
2020-02-25 18:11:00 +0300

    Increasing of encapsulation: GetEmployeeListQueryHandler class is now nested

--- TMS.Application/Employees/Queries/GetEmployeeList/GetEmployeeListQuery.cs
+++ TMS.Application/Employees/Queries/GetEmployeeList/GetEmployeeListQuery.cs
@@ -1,8 +1,41 @@
-﻿using MediatR;
+﻿using AutoMapper.QueryableExtensions;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using TMS.Application.Common.Interfaces;
 
 namespace TMS.Application.Employees.Queries.GetEmployeeList
 {
     public class GetEmployeeListQuery : IRequest<EmployeeListVm>
     {
+        public class GetEmployeeListQueryHandler : IRequestHandler<GetEmployeeListQuery, EmployeeListVm>
+        {
+            private readonly IApplicationDbContext _context;
+            private readonly IMapper _mapper;
+
+            public GetEmployeeListQueryHandler(IApplicationDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<EmployeeListVm> Handle(GetEmployeeListQuery request, CancellationToken cancellationToken)
+            {
+                var employees = await _context.Employees
+                    .ProjectTo<EmployeeDto>(_mapper.ConfigurationProvider)
+                    .OrderBy(p => p.FullName)
+                    .ToListAsync(cancellationToken);
+
+                var vm = new EmployeeListVm
+                {
+                    Employees = employees,
+                };
+
+                return vm;
+            }
+        }
     }
 }
