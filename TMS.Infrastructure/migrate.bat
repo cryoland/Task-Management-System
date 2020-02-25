b17043f5b4febb5651d4577fab577f50e7e2327e
Cryoland <vsrknv@gmail.com>
2020-02-25 13:35:13 +0300

    Migrations assembler added

---
+++ TMS.Infrastructure/migrate.bat
@@ -0,0 +1,2 @@
+﻿@echo off
+dotnet ef migrations add Init --startup-project ../TMS.WebUI-test/TMS.WebUI-test.csproj --context TMS.Infrastructure.Persistence.ApplicationDbContext -o Persistence/Migrations
