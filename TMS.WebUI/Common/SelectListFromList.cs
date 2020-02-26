41f72d8c1b9e59625bc3493173e2895ed875d6e3
Cryoland <vsrknv@gmail.com>
2020-02-26 15:47:49 +0300

    List to SelectList extension added

---
+++ TMS.WebUI/Common/SelectListFromList.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using TMS.Application.Common.Models;
+
+namespace TMS.WebUI.Common
+{
+    public static class SelectListFromList
+    {
+        public static SelectList ToSelectList<T>(this IList<T> list)
+        {
+            return new SelectList(list, nameof(FrameDto.Value), nameof(FrameDto.Name), list.FirstOrDefault());
+        }
+        public static SelectList ToSelectList<T>(this IList<T> list, object selectedValue)
+        {
+            return new SelectList(list, nameof(FrameDto.Value), nameof(FrameDto.Name), selectedValue ?? list.FirstOrDefault());
+        }
+    }
+}
