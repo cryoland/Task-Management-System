b2d064da363eb31203b0d444cae4a11c76d5ba17
Cryoland <vsrknv@gmail.com>
2020-02-26 18:06:30 +0300

    UpdateEmployeeCommand added

---
+++ TMS.Application/Employees/Commands/UpdateEmployee/UpdateEmployeeCommand.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TMS.Application.Common.Exceptions;
+using TMS.Application.Common.Interfaces;
+using TMS.Domain.Entities;
+
+namespace TMS.Application.Employees.Commands.UpdateEmployee
+{
+    public class UpdateEmployeeCommand : IRequest
+    {
+        public long Id { get; set; }
+
+        public string ShortName { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public int RoleId { get; set; }
+
+        public class UpdateEmployeeCommandHandler : IRequestHandler<UpdateEmployeeCommand>
+        {
+            private readonly IApplicationDbContext _context;
+
+            public UpdateEmployeeCommandHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(UpdateEmployeeCommand request, CancellationToken cancellationToken)
+            {
+                var entity = await _context.Employees.FindAsync(request.Id);
+
+                if (entity == null)
+                {
+                    throw new NotFoundException(nameof(Employee), request.Id);
+                }
+
+                entity.ShortName = request.ShortName;
+                entity.FullName = request.FullName;
+                entity.Email = request.Email;
+                entity.Password = request.Password;
+                entity.RoleId = request.RoleId;
+
+                await _context.SaveChangesAsync(cancellationToken);
+
+                return Unit.Value;
+            }
+        }
+    }
+}
---
+++ TMS.Application/Employees/Commands/UpdateEmployee/UpdateEmployeeCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace TMS.Application.Employees.Commands.UpdateEmployee
+{
+    public class UpdateEmployeeCommandValidator : AbstractValidator<UpdateEmployeeCommand>
+    {
+        public UpdateEmployeeCommandValidator()
+        {
+            RuleFor(e => e.Email)
+                .NotEmpty();
+            RuleFor(e => e.Password)
+                .NotEmpty();
+            RuleFor(e => e.FullName)
+                .NotEmpty();
+        }
+    }
+}
