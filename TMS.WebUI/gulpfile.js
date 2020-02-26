b2d000e026851aac23da322f62d3603c7382b089
Cryoland <vsrknv@gmail.com>
2020-02-26 12:38:38 +0300

    Add static

---
+++ TMS.WebUI/gulpfile.js
@@ -0,0 +1,15 @@
+﻿/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+    sass = require("gulp-sass"); // добавляем модуль sass
+
+var paths = {
+    webroot: "./wwwroot/"
+};
+// регистрируем задачу для конвертации файла scss в css
+gulp.task("SASS", function () {
+    return gulp.src('Styles/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(paths.webroot + '/css'));
+});
