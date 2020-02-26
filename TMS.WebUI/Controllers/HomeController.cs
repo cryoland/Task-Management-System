b1cd7d07d5c3367af97b343750f7746858eae175
Cryoland <vsrknv@gmail.com>
2020-02-26 18:06:58 +0300

    Namespace fixed

--- TMS.WebUI/Controllers/HomeController.cs
+++ TMS.WebUI/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebUI.Controllers
+namespace TMS.WebUI.Controllers
 {
     [Authorize]
     public class HomeController : Controller
