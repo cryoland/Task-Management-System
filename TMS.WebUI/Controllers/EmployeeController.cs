6a246734eb2cc9867e4838b1365dbc6f3ad07e42
Cryoland <vsrknv@gmail.com>
2020-02-26 12:49:17 +0300

    Employee mvc controller actions implemented

--- TMS.WebUI/Controllers/EmployeeController.cs
+++ TMS.WebUI/Controllers/EmployeeController.cs
@@ -1,43 +1,30 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using TMS.Application.Employees.Commands.CreateEmployee;
 using TMS.Application.Employees.Queries.GetEmployeeDetail;
 using TMS.Application.Employees.Queries.GetEmployeeList;
 
 namespace TMS.WebUI.Controllers
 {
-    public class EmployeeController : ApiController
+    public class EmployeeController : ContentController
     {
-        [HttpGet]
-        public async Task<ActionResult<EmployeeListVm>> Index()
+        public async Task<ActionResult> Index()
         {            
             return View(await Mediator.Send(new GetEmployeeListQuery()));
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<EmployeeDetailVm>> Detailed(int id)
+        public async Task<ActionResult> Detailed([FromRoute]int id)
         {
-            return await Mediator.Send(new GetEmployeeDetailQuery { EmployeeId = id });
+            return View(await Mediator.Send(new GetEmployeeDetailQuery { EmployeeId = id }));
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<EmployeeDetailVm>> Edit(int id)
+        public async Task<ActionResult> Edit([FromRoute]int id)
         {
-            return await Mediator.Send(new GetEmployeeDetailQuery { EmployeeId = id });
+            return View(await Mediator.Send(new GetEmployeeDetailQuery { EmployeeId = id }));
         }
 
-        [HttpGet]
-        public IActionResult Create()
+        public async Task<ActionResult> Create()
         {
-            return View(new { });
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<long>> Create(CreateEmployeeCommand command)
-        {
-            return await Mediator.Send(command);
+            return View(await Mediator.Send(new GetEmptyEmployeeQuery()));
         }
     }
 }
