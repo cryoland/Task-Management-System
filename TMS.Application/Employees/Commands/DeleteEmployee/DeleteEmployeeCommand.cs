d010d5835546f5c09e3e24ff078fc055e721561a
Cryoland <vsrknv@gmail.com>
2020-02-26 18:06:20 +0300

    DeleteEmployeeCommand added

---
+++ TMS.Application/Employees/Commands/DeleteEmployee/DeleteEmployeeCommand.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TMS.Application.Common.Exceptions;
+using TMS.Application.Common.Interfaces;
+using TMS.Domain.Entities;
+
+namespace TMS.Application.Employees.Commands.DeleteEmployee
+{
+    public class DeleteEmployeeCommand : IRequest
+    {
+        public long Id { get; set; }
+
+        public class DeleteEmployeeCommandHandler : IRequestHandler<DeleteEmployeeCommand>
+        {
+            private readonly IApplicationDbContext _context;
+
+            public DeleteEmployeeCommandHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
+            {
+                var entity = await _context.Employees.FindAsync(request.Id);
+
+                if (entity == null)
+                {
+                    throw new NotFoundException(nameof(Employee), request.Id);
+                }
+
+                _context.Employees.Remove(entity);
+
+                await _context.SaveChangesAsync(cancellationToken);
+
+                return Unit.Value;
+            }
+        }
+    }
+}
