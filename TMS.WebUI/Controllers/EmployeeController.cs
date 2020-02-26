14168eeb6d05260bba0547c131154c5d75feff4c
Cryoland <vsrknv@gmail.com>
2020-02-26 18:16:55 +0300

    MVC EmployeeController CUD methods implemented

--- TMS.WebUI/Controllers/EmployeeController.cs
+++ TMS.WebUI/Controllers/EmployeeController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using TMS.Application.Employees.Commands.CreateEmployee;
+using TMS.Application.Employees.Commands.DeleteEmployee;
+using TMS.Application.Employees.Commands.UpdateEmployee;
 using TMS.Application.Employees.Queries.GetEmployeeDetail;
 using TMS.Application.Employees.Queries.GetEmployeeList;
 
@@ -25,6 +28,35 @@ namespace TMS.WebUI.Controllers
         public async Task<ActionResult> Create()
         {
             return View(await Mediator.Send(new GetEmptyEmployeeQuery()));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateEmployeeCommand command)
+        {
+            var result = await Mediator.Send(command);
+            if(result > 0)
+            {
+                return RedirectToAction(nameof(this.Index));
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Update(long id, UpdateEmployeeCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+            await Mediator.Send(command);
+            return RedirectToAction(nameof(this.Index));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(long id)
+        {
+            await Mediator.Send(new DeleteEmployeeCommand { Id = id });
+            return RedirectToAction(nameof(this.Index));
         }
     }
 }
