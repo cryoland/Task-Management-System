c1c7ba0b04d071e293ba7ec8afa0a7f5f825664f
Cryoland <vsrknv@gmail.com>
2020-02-27 16:10:02 +0300

    New issue controller

---
+++ TMS.WebUI/Controllers/IssueController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TMS.Application.Issues.Commands.CreateIssue;
+using TMS.Application.Issues.Commands.UpdateIssue;
+using TMS.Application.Issues.Commands.DeleteIssue;
+using TMS.Application.Issues.Queries.GetIssueDetail;
+using TMS.Application.Issues.Queries.GetIssueList;
+
+namespace TMS.WebUI.Controllers
+{
+    public class IssueController : ContentController
+    {
+        public async Task<ActionResult> Index()
+        {
+            return View(await Mediator.Send(new GetIssueListQuery()));
+        }
+
+        public async Task<ActionResult> Detailed([FromRoute]int id)
+        {
+            return View(await Mediator.Send(new GetIssueDetailQuery { IssueId = id }));
+        }
+
+        public async Task<ActionResult> Edit([FromRoute]int id)
+        {
+            return View(await Mediator.Send(new GetIssueDetailForEditQuery { IssueId = id }));
+        }
+
+        public async Task<ActionResult> Create()
+        {
+            return View(await Mediator.Send(new GetEmptyIssueQuery()));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateIssueCommand command)
+        {
+            var result = await Mediator.Send(command);
+            if (result > 0)
+            {
+                return RedirectToAction(nameof(this.Index));
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Update(long id, UpdateIssueCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+            await Mediator.Send(command);
+            return RedirectToAction(nameof(this.Index));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(long id)
+        {
+            await Mediator.Send(new DeleteIssueCommand { Id = id });
+            return RedirectToAction(nameof(this.Index));
+        }
+    }
+}
