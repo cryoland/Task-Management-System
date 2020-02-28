11689de67363a0b5323b238816f841665ea2a49e
Cryoland <vsrknv@gmail.com>
2020-02-28 11:14:11 +0300

    Client-side validation using annotations added

--- TMS.Application/Employees/Queries/GetEmployeeDetail/EmployeeDetailVm.cs
+++ TMS.Application/Employees/Queries/GetEmployeeDetail/EmployeeDetailVm.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using TMS.Application.Common.Mappings;
 using TMS.Application.Common.Models;
@@ -11,14 +13,31 @@ namespace TMS.Application.Employees.Queries.GetEmployeeDetail
 {
     public class EmployeeDetailVm : IMapFrom<Employee>
     {
+        [HiddenInput(DisplayValue = false)]
         public long Id { get; set; }
 
+        [Display(Name = "ShortName")]
+        [Required(ErrorMessage = "Не указаны инициалы")]
         public string ShortName { get; set; }
 
+        [Display(Name = "FullName")]
+        [Required(ErrorMessage = "Не указаны ФИО")]
+        [MinLength(5, ErrorMessage = "Длина ФИО от 5 символов")]
+        [MaxLength(64, ErrorMessage = "Длина ФИО до 64 символов")]
         public string FullName { get; set; }
 
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "Не указан Email")]
+        [MinLength(5, ErrorMessage = "Длина адреса от 5 символов")]
+        [MaxLength(64, ErrorMessage = "Длина адреса до 64 символов")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Не указан пароль")]
+        [MinLength(5, ErrorMessage = "Длина пароля от 5 символов")]
+        [MaxLength(64, ErrorMessage = "Длина пароля до 64 символов")]
         public string Password { get; set; }
 
         public bool Active { get; set; }
@@ -27,6 +46,7 @@ namespace TMS.Application.Employees.Queries.GetEmployeeDetail
 
         public string RoleName { get; set; }
 
+        [Display(Name = "Select Role")]
         public IList<FrameDto> UserRoles =
             Enum.GetValues(typeof(UserRole))
             .Cast<UserRole>()
--- TMS.Application/Issues/Queries/GetIssueDetail/IssueDetailVm.cs
+++ TMS.Application/Issues/Queries/GetIssueDetail/IssueDetailVm.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using TMS.Application.Common.Mappings;
 using TMS.Application.Common.Models;
@@ -11,18 +13,33 @@ namespace TMS.Application.Issues.Queries.GetIssueDetail
 {
     public class IssueDetailVm : IMapFrom<Issue>
     {
+        [HiddenInput(DisplayValue = false)]
         public long Id { get; set; }
 
+        [Display(Name = "Title")]
+        [Required(ErrorMessage = "Не указано название")]
+        [MinLength(5, ErrorMessage = "Длина названия от 5 символов")]
+        [MaxLength(64, ErrorMessage = "Длина названия до 64 символов")]
         public string Name { get; set; }
 
+        [Display(Name = "Description")]
+        [MinLength(5, ErrorMessage = "Длина описания от 5 символов")]
+        [MaxLength(256, ErrorMessage = "Длина описания до 256 символов")]
         public string Description { get; set; }
 
+        [Display(Name = "Status")]
         public IssueStatus Status { get; set; }
 
+        [Display(Name = "Priority")]
+        [Required(ErrorMessage = "Не указан приоритет")]
         public PriorityLevel Priority { get; set; }
 
+        [Display(Name = "Select assignee")]
+        [Required(ErrorMessage = "Не указан исполнитель")]
         public long AssigneeId { get; set; }
 
+        [Display(Name = "Select reporter")]
+        [Required(ErrorMessage = "Не указан автор")]
         public long ReporterId { get; set; }
 
         public string AssigneeName { get; set; }
