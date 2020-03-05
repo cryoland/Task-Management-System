808e5e4bccaaf7db4f993bee97849b6784ce4eb3
Cryoland <vsrknv@gmail.com>
2020-03-05 13:52:38 +0300

    Issue detailed razor page added

---
+++ TMS.WebUI/Pages/Issues/Detailed.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TMS.Application.Issues.Queries.GetIssueDetail;
+
+namespace TMS.WebUI.Issues
+{
+    [Authorize]
+    public class DetailedModel : PageModel
+    {
+        private readonly IMediator _mediator;
+
+        public DetailedModel(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public IssueDetailVm Issue { get; private set; }
+
+        public async Task OnGetAsync(long id)
+        {
+            Issue = await _mediator.Send(new GetIssueDetailQuery { IssueId = id });
+        }
+    }
+}
