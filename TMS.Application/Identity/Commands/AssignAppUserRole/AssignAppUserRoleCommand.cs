b33835282f427302674870736e967b6b023115d9
Cryoland <vsrknv@gmail.com>
2020-03-06 11:20:00 +0300

    AssignAppUserRole command implemented

---
+++ TMS.Application/Identity/Commands/AssignAppUserRole/AssignAppUserRoleCommand.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using TMS.Application.Common.Interfaces;
+using TMS.Application.Common.Models;
+using TMS.Domain.Enumerations;
+
+namespace TMS.Application.Identity.Commands.AssignAppUserRole
+{
+    public class AssignAppUserRoleCommand : IRequest<Result>
+    {
+        public string AppUserId { get; set; }
+
+        public UserRole Role { get; set; }
+
+        class CreateAppUserCommandHandler : IRequestHandler<AssignAppUserRoleCommand, Result>
+        {
+            private readonly IIdentityService _identityService;
+
+            public CreateAppUserCommandHandler(IIdentityService identityService)
+            {
+                _identityService = identityService;
+            }
+
+            public async Task<Result> Handle(AssignAppUserRoleCommand request, CancellationToken cancellationToken)
+            {
+                return await _identityService.AddRoleToUserAsync(request.AppUserId, request.Role.ToString());
+            }
+        }
+    }
+}
---
+++ TMS.Application/Identity/Commands/AssignAppUserRole/AssignAppUserRoleCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace TMS.Application.Identity.Commands.AssignAppUserRole
+{
+    public class AssignAppUserRoleCommandValidator : AbstractValidator<AssignAppUserRoleCommand>
+    {
+        public AssignAppUserRoleCommandValidator()
+        {
+            RuleFor(e => e.AppUserId)
+                .NotEmpty();
+            RuleFor(e => e.Role)
+                .NotEmpty();
+        }
+    }
+}
