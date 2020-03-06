7f0032e1aeaf17bc27307fad886c53e25fbcfe2d
Cryoland <vsrknv@gmail.com>
2020-03-06 17:12:38 +0300

    Employee and Issue controllers reimplemented as api controllers

--- TMS.WebUI/Controllers/EmployeeController.cs
+++ TMS.WebUI/Controllers/EmployeeController.cs
@@ -1,109 +1,80 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Threading.Tasks;
-using TMS.Application.Common.Interfaces;
 using TMS.Application.Employees.Commands.CreateEmployee;
 using TMS.Application.Employees.Commands.DeleteEmployee;
 using TMS.Application.Employees.Commands.UpdateEmployee;
 using TMS.Application.Employees.Queries.GetEmployeeDetail;
 using TMS.Application.Employees.Queries.GetEmployeeList;
+using TMS.Application.Identity.Commands.AssignAppUserRole;
+using TMS.Application.Identity.Commands.CreateAppUser;
 
 namespace TMS.WebUI.Controllers
 {
     [Authorize(Roles = "Admin")]
-    public class EmployeeController : ContentController
+    public class EmployeeController : ApiController
     {
-        private readonly IIdentityService _identityService;
-
-        public EmployeeController(IIdentityService identityService) : base()
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<EmployeeListVm>> GetAll()
         {
-            _identityService = identityService;
+            return Ok(await Mediator.Send(new GetEmployeeListQuery()));
         }
 
-        public async Task<ActionResult> Index()
-        {            
-            return View(await Mediator.Send(new GetEmployeeListQuery()));
-        }
-
-        public async Task<ActionResult> Detailed([FromRoute]int id)
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<EmployeeDetailVm>> Get(long id)
         {
-            return View(await Mediator.Send(new GetEmployeeDetailQuery { EmployeeId = id }));
-        }
-
-        public async Task<ActionResult> Edit([FromRoute]int id)
-        {
-            return View(await Mediator.Send(new GetEmployeeDetailQuery { EmployeeId = id }));
-        }
-
-        public async Task<ActionResult> Create()
-        {
-            return View(await Mediator.Send(new GetEmptyEmployeeQuery()));
+            return Ok(await Mediator.Send(new GetEmployeeDetailQuery { EmployeeId = id }));
         }
 
         [HttpPost]
-        public async Task<ActionResult> Create(CreateEmployeeCommand command)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Create(CreateEmployeeCommand command)
         {
-            var dict = new ModelStateDictionary();
+            var appUserId = await Mediator.Send(new CreateAppUserCommand { Email = command.Email, Password = command.Password });
 
-            if (!await _identityService.IsUserExistAsync(command.Email))
+            if (!string.IsNullOrEmpty(appUserId))
             {
-                var (Result, UserId) = await _identityService.CreateUserAsync(command.Email, command.Password);              
+                await Mediator.Send(new AssignAppUserRoleCommand { AppUserId = appUserId, Role = command.Role });
 
-                if (Result.Succeeded)
+                command.AppUserId = appUserId;
+
+                var result = await Mediator.Send(command);
+
+                if (result > 0)
                 {
-                    await _identityService.AddRoleToUserAsync(UserId, command.Role.ToString());
-                    
-                    command.AppUserId = UserId;
-
-                    var result = await Mediator.Send(command);
-
-                    if (result > 0)
-                    {
-                        return RedirectToAction(nameof(this.Index));
-                    }
-                    else
-                    {
-                        dict.AddModelError("ValidationErr", result.ToString());
-                    }
-                }
-                else
-                {
-                    foreach(var error in Result.Errors)
-                    {
-                        dict.AddModelError("IdentityServiceErr", error);
-                    }
-                    return BadRequest(Result.Errors);
+                    return Ok(result);
                 }
             }
-            else
-            {
-                dict.AddModelError("UserExistErr", $"User with email {command.Email} is already exist!");
-            }
-
-            if (dict.Count == 0)
-            {
-                dict.AddModelError("UnknErr", "Unhandled error");
-            }
-            return BadRequest(dict);
+            return NoContent();
         }
 
-        [HttpPost]
-        public async Task<ActionResult> Update(long id, UpdateEmployeeCommand command)
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Update(long id, UpdateEmployeeCommand command)
         {
             if (id != command.Id)
             {
                 return BadRequest();
             }
+
             await Mediator.Send(command);
-            return RedirectToAction(nameof(this.Index));
+
+            return NoContent();
         }
 
-        [HttpPost]
-        public async Task<ActionResult> Delete(long id)
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(long id)
         {
             await Mediator.Send(new DeleteEmployeeCommand { Id = id });
-            return RedirectToAction(nameof(this.Index));
+            
+            return NoContent();
         }
     }
 }
--- TMS.WebUI/Controllers/IssueController.cs
+++ TMS.WebUI/Controllers/IssueController.cs
@@ -1,64 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using TMS.Application.Issues.Commands.CreateIssue;
-using TMS.Application.Issues.Commands.UpdateIssue;
 using TMS.Application.Issues.Commands.DeleteIssue;
+using TMS.Application.Issues.Commands.UpdateIssue;
 using TMS.Application.Issues.Queries.GetIssueDetail;
 using TMS.Application.Issues.Queries.GetIssueList;
-using Microsoft.AspNetCore.Authorization;
 
 namespace TMS.WebUI.Controllers
 {
     [Authorize]
-    public class IssueController : ContentController
+    public class IssueController : ApiController
     {
-        public async Task<ActionResult> Index()
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IssueListVm>> GetAll()
         {
-            return View(await Mediator.Send(new GetIssueListQuery()));
+            return Ok(await Mediator.Send(new GetIssueListQuery()));
         }
 
-        public async Task<ActionResult> Detailed([FromRoute]int id)
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IssueDetailVm>> Get(long id)
         {
-            return View(await Mediator.Send(new GetIssueDetailQuery { IssueId = id }));
-        }
-
-        public async Task<ActionResult> Edit([FromRoute]int id)
-        {
-            return View(await Mediator.Send(new GetIssueDetailForEditQuery { IssueId = id }));
-        }
-
-        public async Task<ActionResult> Create()
-        {
-            return View(await Mediator.Send(new GetEmptyIssueQuery()));
+            return Ok(await Mediator.Send(new GetIssueDetailQuery { IssueId = id }));
         }
 
         [HttpPost]
-        public async Task<ActionResult> Create(CreateIssueCommand command)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Create(CreateIssueCommand command)
         {
             var result = await Mediator.Send(command);
+
             if (result > 0)
             {
-                return LocalRedirect("~/Issues");
+                return Ok(result);
             }
-            return BadRequest(result);
+
+            return NoContent();
         }
 
-        [HttpPost]
-        public async Task<ActionResult> Update(long id, UpdateIssueCommand command)
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Update(long id, UpdateIssueCommand command)
         {
             if (id != command.Id)
             {
                 return BadRequest();
             }
+
             await Mediator.Send(command);
-            return RedirectToAction(nameof(this.Index));
+
+            return NoContent();
         }
 
-        [HttpPost]
-        public async Task<ActionResult> Delete(long id)
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(long id)
         {
             await Mediator.Send(new DeleteIssueCommand { Id = id });
-            return RedirectToAction(nameof(this.Index));
+
+            return NoContent();
         }
     }
 }
