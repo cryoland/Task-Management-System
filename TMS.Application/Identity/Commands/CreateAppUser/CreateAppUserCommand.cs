4d531ed30238d152e2fbd2c3788f082a6a559b06
Cryoland <vsrknv@gmail.com>
2020-03-06 11:19:44 +0300

    CreateAppUser command implemented

---
+++ TMS.Application/Identity/Commands/CreateAppUser/CreateAppUserCommand.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using TMS.Application.Common.Interfaces;
+
+namespace TMS.Application.Identity.Commands.CreateAppUser
+{
+    public class CreateAppUserCommand : IRequest<string>
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        class CreateAppUserCommandHandler : IRequestHandler<CreateAppUserCommand, string>
+        {
+            private readonly IIdentityService _identityService;
+
+            public CreateAppUserCommandHandler(IIdentityService identityService)
+            {
+                _identityService = identityService;
+            }
+
+            public async Task<string> Handle(CreateAppUserCommand request, CancellationToken cancellationToken)
+            {
+                if (!await _identityService.IsUserExistAsync(request.Email))
+                {
+                    var (Result, UserId) = await _identityService.CreateUserAsync(request.Email, request.Password);
+                    
+                    if (Result.Succeeded)
+                    {
+                        return UserId;
+                    }
+                }
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ TMS.Application/Identity/Commands/CreateAppUser/CreateAppUserCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace TMS.Application.Identity.Commands.CreateAppUser
+{
+    public class CreateAppUserCommandValidator : AbstractValidator<CreateAppUserCommand>
+    {
+        public CreateAppUserCommandValidator()
+        {
+            RuleFor(e => e.Email)
+                .NotEmpty();
+            RuleFor(e => e.Password)
+                .NotEmpty();
+        }
+    }
+}
