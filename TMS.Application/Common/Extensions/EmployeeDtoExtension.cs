53c6691899d78d5c03a1993d0636a8ac012d86bd
Cryoland <vsrknv@gmail.com>
2020-03-11 17:06:11 +0300

    EmployeeDto Extension implemented

---
+++ TMS.Application/Common/Extensions/EmployeeDtoExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using TMS.Application.Common.Interfaces;
+using TMS.Application.Employees.Queries.GetEmployeeList;
+using System.Threading.Tasks;
+
+namespace TMS.Application.Common.Extensions
+{
+    public static class EmployeeDtoExtension
+    {
+        public static async Task<List<EmployeeDto>> ProjectUserAppRoles(this List<EmployeeDto> employeeDtos, IIdentityService identityService)
+        {
+            var userRoles = await identityService.FetchUserRolesAsync();
+
+            for (int i = 0; i < employeeDtos.Count; i++)
+            {
+                employeeDtos[i].RoleName = userRoles.FirstOrDefault(ur => ur.appUserId == employeeDtos[i].AppUserId).role;
+            }
+
+            return employeeDtos;
+        }
+    }
+}
